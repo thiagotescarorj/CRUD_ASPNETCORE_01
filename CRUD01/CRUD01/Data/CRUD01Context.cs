febb7c2f62ca85fccd287511ef199ac53b37a535
Thiago Tescaro <thiago.tescaro@outlook.com>
2020-05-06 21:37:53 -0300

    CRUD com o Scaffolding

---
+++ CRUD01/CRUD01/Data/CRUD01Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CRUD01.Models;
+
+namespace CRUD01.Data
+{
+    public class CRUD01Context : DbContext
+    {
+        public CRUD01Context (DbContextOptions<CRUD01Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CRUD01.Models.Departamento> Departamento { get; set; }
+    }
+}
--- CRUD01/CRUD01/Startup.cs
+++ CRUD01/CRUD01/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using CRUD01.Data;
 
 namespace CRUD01
 {
@@ -33,6 +35,9 @@ namespace CRUD01
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<CRUD01Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CRUD01Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
